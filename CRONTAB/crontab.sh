42fb39cc909f544dfc873ee0b18cb7886983905e
fhumeres <fhumeres@alum.mit.edu>
2021-02-04 01:22:15 -0300

    fixing merge crontab

--- CRONTAB/crontab.sh
+++ CRONTAB/crontab.sh
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-0 13 * * * papermill /home/jovyan/work/ETL/covid-chile@min-ciencias.ipynb /home/jovyan/work/CRONTAB/logs/covid-chile@min-ciencias_`date +\%Y\%m\%d\%H\%M\%S`.ipynb
-45 13 * * * papermill /home/jovyan/work/ETL/chile-covid-derived@min-ciencias.ipynb /home/jovyan/work/CRONTAB/logs/covid-chile-derived@min-ciencias_`date +\%Y\%m\%d\%H\%M\%S`.ipynb
-30 14 * * * papermill /home/jovyan/work/ETL/DEIS.ipynb /home/jovyan/work/CRONTAB/logs/DEIS_`date +\%Y\%m\%d\%H\%M\%S`.ipynb
-0 15 * * * papermill /home/jovyan/work/ETL/Cuarentenas.ipynb /home/jovyan/work/CRONTAB/logs/Cuarentenas_`date +\%Y\%m\%d\%H\%M\%S`.ipynb
-30 15 * * * papermill /home/jovyan/work/ETL/consolidado_nacional.ipynb /home/jovyan/work/CRONTAB/logs/consolidado_nacional_`date +\%Y\%m\%d\%H\%M\%S`.ipynb
-0 16 * * *  papermill /home/jovyan/work/ETL/LOGLOG.ipynb /home/jovyan/work/CRONTAB/logs/LOGLOG_`date +\%Y\%m\%d\%H\%M\%S`.ipynb
-0 0 * * MON papermill /home/jovyan/work/ETL/movilidad.ipynb /home/jovyan/work/CRONTAB/logs/movilidad_`date +\%Y\%m\%d\%H\%M\%S`.ipynb
-=======
 0 19 * * * papermill /home/jovyan/work/ETL/covid-chile@min-ciencias.ipynb /home/jovyan/work/CRONTAB/logs/covid-chile@min-ciencias_`date +\%Y\%m\%d\%H\%M\%S`.ipynb
 40 19 * * * papermill /home/jovyan/work/ETL/chile-covid-derived@min-ciencias.ipynb /home/jovyan/work/CRONTAB/logs/covid-chile-derived@min-ciencias_`date +\%Y\%m\%d\%H\%M\%S`.ipynb
 15 20 * * * papermill /home/jovyan/work/ETL/DEIS.ipynb /home/jovyan/work/CRONTAB/logs/DEIS_`date +\%Y\%m\%d\%H\%M\%S`.ipynb
@@ -14,6 +5,5 @@
 0 20 * * * papermill /home/jovyan/work/ETL/consolidado_nacional.ipynb /home/jovyan/work/CRONTAB/logs/consolidado_nacional_`date +\%Y\%m\%d\%H\%M\%S`.ipynb
 10 20 * * *  papermill /home/jovyan/work/ETL/LOGLOG.ipynb /home/jovyan/work/CRONTAB/logs/LOGLOG_`date +\%Y\%m\%d\%H\%M\%S`.ipynb
 0 0 * * SAT papermill /home/jovyan/work/ETL/movilidad.ipynb /home/jovyan/work/CRONTAB/logs/movilidad_`date +\%Y\%m\%d\%H\%M\%S`.ipynb
->>>>>>> 70cf2e560e69be7e72185a1a61e5da0c3cdc79cd
 26 */6 * * * papermill /home/jovyan/work/ETL/cleanETL.ipynb /home/jovyan/work/CRONTAB/logs/cleanETL_`date +\%Y\%m\%d\%H\%M\%S`.ipynb
 45 22 * * * papermill /home/jovyan/work/ETL/WORLD.ipynb /home/jovyan/work/CRONTAB/logs/WORLD_`date +\%Y\%m\%d\%H\%M\%S`.ipynb
